f8fbc550e0077a27f22b7457365117c5511fdf41
Peter Han <peter.han.9493@gmail.com>
2021-08-28 19:02:26 -0700

    Remove intents

--- src-discord-starboard-bot/intents.ts
+++ src-discord-starboard-bot/intents.ts
@@ -1,8 +1,1 @@
-import { Intents } from "discord.js";
-
-module.exports = [
-  Intents.FLAGS.GUILDS,
-  Intents.FLAGS.GUILD_MESSAGES,
-  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  Intents.FLAGS.GUILD_WEBHOOKS,
-];
+module.exports = [];
